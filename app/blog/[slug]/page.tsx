79945da9fbc39550c31bc03e91737cf4ffd94852
John Pate <john.pate.programmer@gmail.com>
2024-05-08 19:04:26 +0800

    ddd

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -40,7 +40,7 @@ export default function PostPage({ params }: Props) {
           <div className="pb-4 z-10 flex flex-col text-white">
             <div className="blog-content" dangerouslySetInnerHTML={{ __html: featured.body }} />
           </div>
-          {slug !== "introduction" ? (
+          {slug !== "about-family-and-friends" ? (
             <div className="pb-4 z-10 flex flex-col items-center">
               <a
                 href={`/blog`}
@@ -53,18 +53,18 @@ export default function PostPage({ params }: Props) {
             <div className="flex">
               <div className="pb-4 z-10 flex flex-col items-center">
                 <a
-                  href={`/blog/jacobs-identities`}
+                  href={`/about`}
                   className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
                 >
-                  Frameworks of My Identity
+                  About Me
                 </a>
               </div>
               <div className="pb-4 z-10 flex flex-col items-center">
                 <a
-                  href={`/blog/jacobs-hobbies`}
+                  href={`/about/history`}
                   className="text-white border border-white rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none hover:bg-white hover:text-black"
                 >
-                  Discovering My World
+                  Summary of My History
                 </a>
               </div>
             </div>
